56265c227e76be66bf24b7569241e690c1b67d0e
JuHaNi654 <j.turpeinen20@gmail.com>
2022-08-14 15:53:28 +0300

    Added logic to the Login route

--- server/routes/auth_route.go
+++ server/routes/auth_route.go
@@ -1,17 +1,51 @@
 package routes
 
 import (
+	"context"
+	"log"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
 type Auth struct {
-	Email    string
-	Password string
+	Email    string `json:"email"`
+	Password string `json:"password"`
 }
 
 func Login(ctx *gin.Context, db *mongo.Client) {
-	ctx.SecureJSON(http.StatusCreated, gin.H{"msg": "ok"})
+	var body Auth
+	var result Auth
+	if err := ctx.BindJSON(&body); err != nil {
+		ctx.SecureJSON(http.StatusInternalServerError, gin.H{
+			"msg": "Something went wrong",
+		})
+	}
+
+	filter := bson.D{{"email", body.Email}}
+	coll := db.Database("chat").Collection("users")
+	err := coll.FindOne(context.TODO(), filter).Decode(&result)
+
+	if err != nil {
+		if err == mongo.ErrNoDocuments {
+			log.Println("Email not found")
+			ctx.SecureJSON(http.StatusUnauthorized, gin.H{"msg": "Invalid email or password"})
+			return
+		}
+
+		ctx.SecureJSON(http.StatusInternalServerError, gin.H{
+			"msg": "Something went wrong",
+		})
+		return
+	}
+
+	if result.Password != body.Password {
+		log.Println("Invalid password")
+		ctx.SecureJSON(http.StatusUnauthorized, gin.H{"msg": "Invalid email or password"})
+		return
+	}
+
+	ctx.SecureJSON(http.StatusCreated, body)
 }
