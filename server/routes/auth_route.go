49b9a07bceaa43f672542f2db9f9adb92ee584a0
JuHaNi654 <j.turpeinen20@gmail.com>
2022-08-14 16:52:52 +0300

    added golang-jwt pkg and updated login route that generates now jwt token after successfully login

--- server/routes/auth_route.go
+++ server/routes/auth_route.go
@@ -2,9 +2,12 @@ package routes
 
 import (
 	"context"
+	"log"
 	"net/http"
+	"os"
 
 	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt/v4"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 )
@@ -14,9 +17,15 @@ type Auth struct {
 	Password string `json:"password"`
 }
 
+type UserToken struct {
+	Id       string `json:"id"`
+	Username string `json:"username"`
+}
+
 func Login(ctx *gin.Context, db *mongo.Client) {
 	var body Auth
 	var result Auth
+	var err error
 	if err := ctx.BindJSON(&body); err != nil {
 		ctx.SecureJSON(http.StatusInternalServerError, gin.H{
 			"msg": "Something went wrong",
@@ -25,7 +34,7 @@ func Login(ctx *gin.Context, db *mongo.Client) {
 
 	filter := bson.D{{"email", body.Email}}
 	coll := db.Database("chat").Collection("users")
-	err := coll.FindOne(context.TODO(), filter).Decode(&result)
+	err = coll.FindOne(context.TODO(), filter).Decode(&result)
 
 	if err != nil {
 		if err == mongo.ErrNoDocuments {
@@ -44,5 +53,23 @@ func Login(ctx *gin.Context, db *mongo.Client) {
 		return
 	}
 
-	ctx.SecureJSON(http.StatusCreated, body)
+	var payload UserToken
+	err = coll.FindOne(context.TODO(), filter).Decode(&payload)
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"id":  payload.Id,
+		"exp": 21600,
+	})
+
+	secret := []byte(os.Getenv("JWT_SECRET"))
+	tokenString, err := token.SignedString(secret)
+	if err != nil {
+		log.Printf("Error occurred while creating jwt: %v\n", err)
+		ctx.SecureJSON(http.StatusInternalServerError, gin.H{
+			"msg": "Something went wrong",
+		})
+		return
+	}
+
+	ctx.SecureJSON(http.StatusCreated, gin.H{"token": tokenString, "username": payload.Username})
 }
